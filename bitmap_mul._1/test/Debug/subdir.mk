dec4d7ac154b4a11797e9c60a16c5ad2816e191a
jasmine8128 <jasmine8128@163.com>
2016-03-17 18:49:13 +0800

    function

---
+++ bitmap_mul._1/test/Debug/subdir.mk
@@ -0,0 +1,25 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CU_SRCS += \
+../test1.cu 
+
+CU_DEPS += \
+./test1.d 
+
+OBJS += \
+./test1.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cu
+	@echo 'Building file: $<'
+	@echo 'Invoking: NVCC Compiler'
+	/usr/local/cuda-7.0/bin/nvcc -G -g -O0 -gencode arch=compute_30,code=sm_30  -odir "." -M -o "$(@:%.o=%.d)" "$<"
+	/usr/local/cuda-7.0/bin/nvcc -G -g -O0 --compile --relocatable-device-code=false -gencode arch=compute_30,code=compute_30 -gencode arch=compute_30,code=sm_30  -x cu -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
